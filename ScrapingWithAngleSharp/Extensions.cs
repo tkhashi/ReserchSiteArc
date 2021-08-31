5d59b0d46e22a5327e5246c9e35e6b7f3542bbf3
KazuhiroTakahashi <k_takahashi@just-ltd.co.jp>
2021-08-31 18:22:20 +0900

    Can scrape divided by frame elemnent

--- ScrapingWithAngleSharp/Extensions.cs
+++ ScrapingWithAngleSharp/Extensions.cs
@@ -1,11 +1,61 @@
-﻿namespace ScrapingWithAngleSharp
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using AngleSharp.Dom;
+
+namespace ScrapingWithAngleSharp
 {
     public static class Extensions
     {
+        public static IEnumerable<string> GetElementByAttr(this IDocument document, string tag, string attr)
+        {
+            return document
+                    .QuerySelectorAll(tag)
+                    .Select(cell => cell.GetAttribute(attr));
+        }
+
         public static string GetFullPath(this string domain, string path)
         {
             return domain + path;
         }
-        
+
+        public static List<string> AddFlagUrls(this List<string> urlList, string baseUrl, IEnumerable<string> links,
+            string regex)
+        {
+            var addLinks = links
+                .Where(link => link != null)
+                .Where(link => Regex.IsMatch(link, regex))
+                .Select(link => baseUrl.GetFullPath(link))
+                .ToList();
+            urlList.AddRange(addLinks);
+
+            return urlList;
+        }
+
+        public static List<string> AddRelativePathUrls(this List<string> urlList, string domain,
+            IEnumerable<string> links, string regex)
+        {
+            var addLinks = links
+                .Where(link => link != null)
+                .Where(link => Regex.IsMatch(link, regex))
+                .Select(link => domain.GetFullPath(link))
+                .ToList();
+
+            urlList.AddRange(addLinks);
+
+            return urlList;
+        }
+
+        public static List<string> AddFullPathUrls(this List<string> urlList, IEnumerable<string> links, string regex)
+        {
+            var addLinks = links
+                .Where(link => link != null)
+                .Where(link => Regex.IsMatch(link, regex))
+                .ToList();
+
+            urlList.AddRange(addLinks);
+
+            return urlList;
+        }
     }
 }
--- ScrapingWithAngleSharp/Program.cs
+++ ScrapingWithAngleSharp/Program.cs
@@ -16,42 +16,64 @@ namespace ScrapingWithAngleSharp
             var config = Configuration.Default.WithDefaultLoader();
             var context = BrowsingContext.New(config);
             //対象のアドレスをconfigの設定で開く？
-            var baseUrl = "https://blog.okazuki.jp/entry/2017/07/23/165809";
+            var baseUrl = "http://abehiroshi.la.coocan.jp";
+            if (!Regex.IsMatch(baseUrl, @"$/")) baseUrl += @"/";
             var domain = Regex.Match(baseUrl, @"https?://[^/]+/?").Value;
             var http = Regex.Match(baseUrl, @"https?:").Value;
             var document = await context.OpenAsync(baseUrl);
-            //対象の要素を取得
-            var aTag = "a";
-            var cells = document.QuerySelectorAll(aTag);
-            //取得した要素のhref属性を取得
-            var links = cells.Select(cell => cell.GetAttribute("href"));
+            //対象の要素・属性を取得
+            var links = document.GetElementByAttr("a", "href");
+            var frameLinks = document.GetElementByAttr("frame", "src");
 
-            var flagUrls = links
-                .Where( link => link != null)
-                .Where(link => Regex.IsMatch(link, @"^#"))
-                .Select(link => baseUrl.GetFullPath(link))
-                .ToList();
-            var relativePaths = links
-                .Where( link => link != null)
-                .Where(link => Regex.IsMatch(link, @"^[^/][^http][^#]"))
-                .Select(link => domain.GetFullPath(link))
-                .ToList();
-            var fullPaths = links
-                .Where( link => link != null)
-                .Where(link => Regex.IsMatch(link, @"^http"))
-                .ToList();
-
-            var urlList = new List<string>();
-            urlList.AddRange(flagUrls);
-            urlList.AddRange(relativePaths);
-            urlList.AddRange(fullPaths);
+            var flagRegex = @"^#";
+            var relativeRegex = @"^[^/][^http][^#]";
+            var fullPathRegex = @"^http";
+            var urlList = new List<string>()
+                .AddFlagUrls(baseUrl, links, flagRegex)
+                .AddRelativePathUrls(domain, links, relativeRegex)
+                .AddFullPathUrls(links, fullPathRegex);
 
             foreach (var url in urlList)
             {
                 Console.WriteLine(url);
             }
 
-        }
+            var frameUrlList = new List<string>()
+                .AddRelativePathUrls(domain, frameLinks, relativeRegex);
 
+            foreach (var frameUrl in frameUrlList)
+            {
+                //frame内のa/hrefを取得
+                var documentInFrame = await context
+                    .OpenAsync(frameUrl);
+                var linksInFrame = documentInFrame.GetElementByAttr("a", "href");
+                var urlListInFrame = new List<string>()
+                    .AddFlagUrls(baseUrl, linksInFrame, flagRegex)
+                    .AddRelativePathUrls(domain, linksInFrame, relativeRegex)
+                    .AddFullPathUrls(linksInFrame, fullPathRegex);
+                foreach (var url in urlListInFrame)
+                {
+                    Console.WriteLine(url);
+                }
+            }
+
+            //var urlListInFrame = new List<string>()
+            //    .AddRelativePathUrls(domain, frameLinks, relativeRegex)
+            //    .Select(async frameUrl =>
+            //    {
+            //        var documentInFrame = await context.OpenAsync(frameUrl);
+            //        var linksInFrame = documentInFrame.GetElementByAttr("a", "href");
+
+            //        return new List<string>()
+            //            .AddFlagUrls(baseUrl, linksInFrame, flagRegex)
+            //            .AddRelativePathUrls(domain, linksInFrame, relativeRegex)
+            //            .AddFullPathUrls(linksInFrame, fullPathRegex);
+            //    });
+            //foreach (var url in urlListInFrame)
+            //{
+            //    Console.WriteLine(url);
+            //}
+
+        }
     }
 }
